b0e54911e9fbe0a86fbb76886d86da4a86e08d9b
shafiqmh <mohammed.shafiq@academytrainee.com>
2019-08-20 13:11:13 +0100

    Initial Commit

---
+++ src/main/java/com/qa/App.java
@@ -0,0 +1,15 @@
+package com.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println(args[0]);
+        System.out.println( "Hello World!" );
+    }
+}
